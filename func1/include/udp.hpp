0b8c7abf39fbbccfa871de676fdefe92b0ca5b10
Satoshi Kanda <none>
2023-01-28 12:35:55 +0900

    初期設定

---
+++ func1/include/udp.hpp
@@ -0,0 +1,3 @@
+#ifndef _UDP_HPP_
+#define _UDP_HPP_
+#endif
---
+++ send.cpp
@@ -0,0 +1,26 @@
+#include <winsock2.h>
+
+int
+main()
+{
+ WSAData wsaData;
+
+ SOCKET sock;
+ struct sockaddr_in addr;
+
+ WSAStartup(MAKEWORD(2,0), &wsaData);
+
+ sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+ addr.sin_family = AF_INET;
+ addr.sin_port = htons(12345);
+ addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+ sendto(sock, "HELLO", 5, 0, (struct sockaddr *)&addr, sizeof(addr));
+
+ closesocket(sock);
+
+ WSACleanup();
+
+ return 0;
+}
